cef740174f86cab6c537685710f5ba86c59b26a7
Bethlydia Mutuba <mutubaneema@gmail.com>
2024-02-26 16:47:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const home = require("./routes/home");
 const dotenv = require('dotenv').config();
 
+
 // Middlewares
 const app = express();
 app.use(express.json());                  
 
 // Routes
-app.use("/block-transactions", home);
-
+app.use("/confirm-pin", home);
 
 // connection
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
