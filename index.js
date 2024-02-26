cf6a35fdcdfafc63218d7713ba6220ffce3078cd
bmutuba <bmutuba@safaricom.co.ke>
2024-02-26 16:31:53 +0300

    ok

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
-const blockTransactions = require("./routes/blockTransactions");
 const dotenv = require('dotenv').config();
-
 
 // Middlewares
 const app = express();
 app.use(express.json());                  
 
 // Routes
-app.use("/confirm-pin", home);
-app.use("/block-transactions", blockTransactions)
+app.use("/block-transactions", home);
 
 
 // connection
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
